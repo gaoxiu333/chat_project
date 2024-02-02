2160bd7caded41c89ccefa81cb75d4d5d3e3bd6c
gaoxiu <gaoxiu333@gmail.com>
2024-02-02 10:04:45 +0800

    icons

--- packages/front_end/src/router/index.tsx
+++ packages/front_end/src/router/index.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import Home from '../pages/Home';
 import {Login} from '../pages/Login';
 import {Chat} from '../pages/Chat';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const screenOptions = {
+const screenOptions = ({route}: any) => ({
   headerShown: false,
-};
+  tabBarIcon: ({focused, color, size}: any) => {
+    let iconName = 'home';
+    if (route.name === 'Home') {
+      iconName = focused ? 'home' : 'home-outline';
+    } else if (route.name === 'Chat') {
+      iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+    }
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+});
 const TabGroup = () => {
   return (
     <Tab.Navigator screenOptions={screenOptions}>
